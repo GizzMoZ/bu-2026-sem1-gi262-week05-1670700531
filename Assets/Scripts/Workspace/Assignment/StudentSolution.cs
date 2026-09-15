0afd0b4e84d31de02da82e5fef108ae8ca25648e
GizzMoZ <MickAriya28@gmail.com>
2026-09-15 13:24:29 +0700

    ltc03

--- Assets/Scripts/Workspace/Assignment/StudentSolution.cs
+++ Assets/Scripts/Workspace/Assignment/StudentSolution.cs
@@ -49,6 +49,17 @@ namespace Assignment
 
         public int[] LCT03_InsertionSortAscending(int[] numbers)
         {
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                int key = numbers[i];
+                int j = i - 1;
+                while (numbers[j] > key && j >= 0)
+                {
+                    numbers[j + 1] = numbers[j];
+                    j--;
+                }
+                numbers[j + 1] = key;
+            }
             return numbers;
         }
 
